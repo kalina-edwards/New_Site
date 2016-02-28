4dfcf06557db2716848a942d0605391b44b2bfba
constsync <timothy.mbc@gmail.com>
2016-02-27 19:26:24 -0500

    General refactoring

--- assets/js/Controllers/MainController.js
+++ assets/js/Controllers/MainController.js
@@ -1,9 +1,9 @@
-/**
- * Created by Tim on 2/26/16.
- */
 app.controller('mainCtrl', ["$scope", function ($scope) {
 
    $scope.doStuff = function () {
        console.log('Soon to be doing stuff :)');
    }
 }]);
+
+
+
